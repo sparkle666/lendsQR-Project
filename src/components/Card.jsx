d2dd22f95e652578e5d79921241900acfadc42aa
Sixtus Anyanwu <sikky606@gmail.com>
2022-07-06 11:44:16 +0100

    updated card.jsx login.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import { BsDisplay } from "react-icons/bs";
+import { FaUsers } from "react-icons/fa";
 
 const Card = ({ title, stats, icon }) => {
   return (
     <div className="card">
       {icon ? (
-        <span> {icon} </span>
+        <div
+          style={{
+            backgroundColor: "pink",
+            // padding: "8px",
+            width: "25px",
+            height: "25px",
+            borderRadius: "50%",
+            textAlign: "center",
+          }}
+        >
+          <span style={{ display: "flex", alignItems: "center" }}>{icon}</span>
+        </div>
       ) : (
         // <CheckCircleTwoTone className="card__icon" />""
-        ""
+        <span style={{ backgroundColor: "pink", padding: "10px" }}>
+          <FaUsers />
+        </span>
       )}
       <p className="card__title"> {title} </p>
       <p className="card__stats">{stats.toLocaleString()}</p>
--- src/components/UserContent.jsx
+++ src/components/UserContent.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Card from './Card';
 import CardList from './CardList';
 import Table from "./Table"
 import TableFooter from './TableFooter';
 import TableFilter from './TableFilter';
 import TablePagination from './TablePagination';
+import {FaUsers} from "react-icons/fa"
 
 const UserContent = () => {
+  const cardRef = useRef()
+  let height = cardRef.current.getAttribute("fill")
+  console.log(height)
+
   return (
     <div className = "user__content">
       <p> USERS </p>
       <CardList>
-        <Card title="Users" stats={2390} />
+        <Card ref = {cardRef} title="Users" stats={2390} icon = {<FaUsers style={{padding: "8px 0 8px 0", color: "green"}}/>} />
         <Card title="Active Users" stats={2342} />
         <Card title="Users With Loan" stats={12898} />
         <Card title="Users With Savings" stats={128998} />
